1e52fefcc02c1768aff73905681c02e30ff1fa8e
cvdana <cvdana99@gmail.com>
2023-07-10 14:05:19 +0200

    made geolocation work

--- src/Weather.js
+++ src/Weather.js
@@ -13,6 +13,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
   const [ready, setReady] = useState(false);
+
   const [weatherData, setWeatherData] = useState({});
   function handleResponse(response) {
     setWeatherData({
@@ -41,11 +42,24 @@ export default function Weather(props) {
   function handleCityChange(event) {
     setCity(event.target.value);
   }
+  function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const apiKey = `94413t4dbc141o4dc71ce00caf84f31e`;
+    const units = `metric`;
+    const apiUrlLocation = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=${units}`;
+    axios.get(apiUrlLocation).then(handleResponse);
+  }
+  function getCurrentPosition(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+
   if (ready) {
     return (
       <div className="Weather">
         <div className="row justify-content-center seekBlock">
-          <div className="col-3 mt-2 location">
+          <div className="col-3 mt-2 location" onClick={getCurrentPosition}>
             <FontAwesomeIcon
               icon={faLocationDot}
               size="2xl"
