ddb94836f9794624a6ee40f618e24f69f327ecda
cvdana <cvdana99@gmail.com>
2023-07-10 12:23:39 +0200

    made more media query changes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,11 +17,11 @@ export default function WeatherInfo(props) {
           <WeatherIcon code={props.data.icon} size={100} />
         </div>
         <div className="row justify-content-center ">
-          <div className="col-4 text-center  maintemp">
+          <div className="col-sm-4  text-center  maintemp">
             <WeatherTemperature celsius={props.data.temperature} />
 
             <div className="w-100"></div>
-            <h4 className="col condition">{props.data.description}</h4>
+            <h4 className="col-sm condition">{props.data.description}</h4>
           </div>
           <div className=" col-3 d-none d-md-block extraInformation ">
             <h4 className="details d-none d-md-block	 ">DETAILS</h4>
@@ -33,20 +33,18 @@ export default function WeatherInfo(props) {
               Humidity {Math.round(props.data.humidity)}%
             </h4>
             <h4 className=" wind d-none d-md-block">
-              {" "}
               Wind {Math.round(props.data.wind)} Km/h
             </h4>
           </div>
         </div>
         <div className=" row text-center extraInformation ">
-          <h4 className="col temperatures d-md-none ">
+          <h4 className="col temperatures d-md-none p-0 ">
             Feels like {Math.round(props.data.feels)}º
           </h4>
-          <h4 className="col  humidity d-md-none">
+          <h4 className="col  humidity d-md-none p-0">
             Humidity {Math.round(props.data.humidity)}%
           </h4>
-          <h4 className="col wind d-md-none ">
-            {" "}
+          <h4 className="col wind d-md-none p-0 ">
             Wind {Math.round(props.data.wind)} Km/h
           </h4>
         </div>
